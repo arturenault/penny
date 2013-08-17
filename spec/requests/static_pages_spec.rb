05fd541d1e6fd387ebb9a0f93c16681c3025f92e
Artur Upton Renault <artur.renault@gmail.com>
2013-08-17 18:53:56 -0300

    Better tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,60 +2,34 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Penny'" do
-      visit root_path
-      expect(page).to have_content('Penny')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Penny")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Penny') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("Penny | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About'" do
-      visit about_path
-      expect(page).to have_content('About')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("Penny | About")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("Penny | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
