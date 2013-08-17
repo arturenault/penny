f3d5facfbb1bbc8bef500341bc892833b8cfd289
Artur Upton Renault <artur.renault@gmail.com>
2013-08-17 18:28:39 -0300

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static pages" do
       expect(page).to have_content('Penny')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Penny | Home")
+      expect(page).to have_title("Penny")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("Penny")
     end
   end
 
@@ -34,7 +39,7 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About')
     end
-    
+
     it "should have the title 'About'" do
       visit '/static_pages/about'
       expect(page).to have_title("Penny | About")
