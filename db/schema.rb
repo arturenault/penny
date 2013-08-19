30e98dabd37060c5a3a0de85e10c1aa10e098a9c
Artur Upton Renault <artur.renault@gmail.com>
2013-08-19 00:36:11 -0300

    Added index to username to avoid duplicate users

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130819025747) do
+ActiveRecord::Schema.define(version: 20130819033458) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
