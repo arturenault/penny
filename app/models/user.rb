e36f825e5a27e7f882d04f9b8144542df456451e
Artur Upton Renault <artur.renault@gmail.com>
2013-08-19 14:59:02 -0300

    Complete user signin/signout operations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
   VALID_USERNAME_REGEX = /\A[a-zA-Z0-9_]+\z/i
   validates :username, presence: true,
                        length: { maximum: 50 },
@@ -6,4 +7,18 @@ class User < ActiveRecord::Base
                        uniqueness: {case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130819033937) do
+ActiveRecord::Schema.define(version: 20130819173659) do
 
   create_table "users", force: true do |t|
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
