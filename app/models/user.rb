f6a604c80ae77e697034a7216742f3d0d47a5543
Artur Upton Renault <artur.renault@gmail.com>
2013-08-25 01:49:45 -0300

    Proto-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   
   def feed
     # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+    Post.where("user_id = ?", id)
   end
 
   private
