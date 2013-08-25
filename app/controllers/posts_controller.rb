627ac0da1586554836abd5f522f3281c2689fe2e
Artur Upton Renault <artur.renault@gmail.com>
2013-08-25 02:13:06 -0300

    Post destruction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+  
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
@@ -12,6 +14,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    redirect_to root_url
   end
 
   def show
@@ -24,5 +28,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 end
