5a4d6727df3e330e8b63146b24e16020fe18d14f
Artur Upton Renault <artur.renault@gmail.com>
2013-08-28 19:44:17 -0300

    Admin privileges

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,6 +22,14 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+  
+  def current_users_post?
+    @post.user == current_user
+  end
+  
+  def admin?
+    current_user.admin
+  end
 
   def signed_in_user
     unless signed_in?
